5ea6cf0bf36a2552d0e2da223e981952872af272
jhunethan <jhunethan23@gmail.com>
2021-07-26 15:46:34 +0100

    added error handling

--- utils/Api.js
+++ utils/Api.js
@@ -3,6 +3,10 @@ import axios from "axios";
 import qs from "qs";
 
 export const getSpotifyToken = async () => {
+  if (!SPOTIFY_CLIENTID || !SPOTIFY_SECRET)
+    return console.error(
+      "ERROR: getSpotifyToken() environment variables must be set"
+    );
   const encoded = btoa(`${SPOTIFY_CLIENTID}:${SPOTIFY_SECRET}`);
   const response = await axios({
     method: "post",
@@ -15,5 +19,5 @@ export const getSpotifyToken = async () => {
       Authorization: `Basic ${encoded}`,
     },
   });
-  return response;
+  return response.data.access_token;
 };
