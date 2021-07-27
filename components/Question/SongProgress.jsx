4590e563912602e93578e08cf42b2e86795aec3f
jhunethan <jhunethan23@gmail.com>
2021-07-27 16:35:22 +0100

    timer only shows when song is played

--- components/Question/SongProgress.jsx
+++ components/Question/SongProgress.jsx
@@ -8,34 +8,43 @@ import { useState } from "react";
 const SongProgress = () => {
   const [duration, setDuration] = useState(5000);
   const [progress, setProgress] = useState(100);
-  const [timer, setTimer] = useState(duration / 1000);
+  const [timer, setTimer] = useState();
+  const [isPlaying, setIsPlaying] = useState(false)
 
   useEffect(() => {
     if (timer && timer > 0) {
+      setIsPlaying(true)
       setTimeout(() => {
         setTimer((currTimer) => {
           return (currTimer - 0.1).toFixed(1);
         });
       }, 100);
+    }else{
+      setIsPlaying(false)
     }
   }, [timer]);
 
   return (
     <View style={AppStyle.container}>
       <Text>Song Progress Bar Here</Text>
-      <AnimatedCircularProgress
-        size={120}
-        width={15}
-        fill={progress}
-        tintColor="#00e0ff"
-        backgroundColor="#3d5875"
-        duration={duration}
-      >
-        {(fill) => {
-          return <Text>{timer}S</Text>;
-        }}
-      </AnimatedCircularProgress>
-      <MediaPlayer songDuration={duration} />
+      {isPlaying && (
+        <AnimatedCircularProgress
+          size={120}
+          width={15}
+          fill={progress}
+          tintColor="#00e0ff"
+          backgroundColor="#3d5875"
+          duration={duration}
+        >
+          {(fill) => {
+            return <Text>{timer}S</Text>;
+          }}
+        </AnimatedCircularProgress>
+      )}
+      <MediaPlayer
+        resetTimer={() => setTimer(duration / 1000)}
+        songDuration={duration}
+      />
     </View>
   );
 };
