0610a2e79bf99cc37b3910a0a14aae225a55d2fa
jhunethan <jhunethan23@gmail.com>
2021-07-27 12:36:23 +0100

    added MediaPlayer component

--- components/Question/SongProgress.jsx
+++ components/Question/SongProgress.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Text, View } from "react-native";
 import AppStyle from "../../style/App.style";
+import MediaPlayer from "./MediaPlayer"
 
 const SongProgress = () => {
   return (
     <View style={AppStyle.container}>
       <Text>Song Progress Bar Here</Text>
+      <MediaPlayer />
     </View>
   );
 };
