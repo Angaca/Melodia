85430a23fe3cc04949e8c7d97728acf27fbe6669
Andrea Catania <andreacatania91@gmail.com>
2021-07-28 16:38:30 +0100

    add context for score

--- App.js
+++ App.js
@@ -1,35 +1,39 @@
 import "react-native-gesture-handler";
-import React from "react";
+import React, { useState } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
+import { ScoreContext } from "./context/ScoreContext";
 import Homepage from "./components/Homepage/Homepage";
 import Question from "./components/Question/Question";
 import Results from "./components/Results/Results";
 
-
 const Stack = createStackNavigator();
 
 export default function App() {
+  const [score, setScore] = useState(0);
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Homepage"
-          component={Homepage}
-          style={{ backgroundColor: "transparent" }}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Question"
-          component={Question}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Results"
-          component={Results}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ScoreContext.Provider value={{ score, setScore }}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Homepage"
+            component={Homepage}
+            style={{ backgroundColor: "transparent" }}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Question"
+            component={Question}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Results"
+            component={Results}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ScoreContext.Provider>
   );
 }
--- components/Results/FinalScore.js
+++ components/Results/FinalScore.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Text, View } from "react-native";
+import { ScoreContext } from "../../context/ScoreContext";
 import AppStyle from "../../style/App.style";
 
 const FinalScore = () => {
+  const { score } = useContext(ScoreContext);
+
   return (
     <View style={AppStyle.container}>
       <Text>Bravo Maestro!</Text>
-      <Text>You got 3 out of 10!</Text>
+      <Text>You got {`${score}`} out of 10!</Text>
     </View>
   );
 };
