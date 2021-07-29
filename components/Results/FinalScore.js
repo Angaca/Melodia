bd74b161c8b60ca64e37659c2660b59b4f0ebf15
Juliette Curran <juliettecurran@hotmail.com>
2021-07-29 11:54:03 +0100

    Styled Results buttons and Score text

--- components/Results/FinalScore.js
+++ components/Results/FinalScore.js
@@ -1,17 +1,34 @@
 import React, { useContext } from "react";
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 import { ScoreContext } from "../../context/ScoreContext";
 import AppStyle from "../../style/App.style";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 const FinalScore = () => {
   const { score } = useContext(ScoreContext);
 
   return (
     <View style={AppStyle.container}>
-      <Text>Bravo Maestro!</Text>
-      <Text>You got {`${score}`} out of 10!</Text>
+      <Text style={style.rankTitle}>Bravo Maestro</Text>
+      <Text style={style.score}>You got {`${score}`} out of 10!</Text>
     </View>
   );
 };
+
+const style = StyleSheet.create({
+  rankTitle: {
+    fontSize: hp("6%"),
+    color: "#E0B318",
+    fontWeight: 800,
+  },
+  score: {
+    fontSize: hp("3%"),
+    color: "white",
+    fontWeight: 800,
+  },
+});
 
 export default FinalScore;
