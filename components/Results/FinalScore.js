082739181579757c8ef18e11dd83566f16a0b508
jhunethan <jhunethan23@gmail.com>
2021-07-29 11:42:24 +0100

    added winning messages based on score

--- components/Results/FinalScore.js
+++ components/Results/FinalScore.js
@@ -5,10 +5,11 @@ import AppStyle from "../../style/App.style";
 
 const FinalScore = () => {
   const { score } = useContext(ScoreContext);
-
+  const resultMessages = ["Booo", "Not quite my tempo...", "Encore!","Bravo Maestro!"];
+  const messageIndex = Math.floor(score / 3)
   return (
     <View style={AppStyle.container}>
-      <Text>Bravo Maestro!</Text>
+      <Text>{resultMessages[messageIndex]}</Text>
       <Text>You got {`${score}`} out of 10!</Text>
     </View>
   );
