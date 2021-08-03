d802acdce3f4212abf248d3b55e9767103e4339b
Juliette Curran <juliettecurran@hotmail.com>
2021-08-03 15:12:42 +0100

    New HomeBtn Lottie component for Results page

---
+++ components/Results/HomeBtn.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import LottieView from "lottie-react-native";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
+
+export default class HomeBtn extends React.Component {
+  componentDidMount() {
+    this.HomeBtn.play();
+  }
+
+  render() {
+    return (
+      <LottieView
+        style={style.HomeBtn}
+        ref={(animation) => {
+          this.HomeBtn = animation;
+        }}
+        source={require("../../style/animations/yellowHouse.json")}
+      />
+    );
+  }
+}
+
+const style = StyleSheet.create({
+  HomeBtn: {
+    height: hp("11%"),
+    width: wp("11%"),
+    marginBottom: hp("2%"),
+  },
+});
