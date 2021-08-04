fe4a43c61add95fdccfc4fcbc5023d6817abc889
Juliette Curran <juliettecurran@hotmail.com>
2021-08-04 15:08:38 +0100

    Amended NextBtn

--- components/Question/NextBtn.js
+++ components/Question/NextBtn.js
@@ -1,46 +1,32 @@
 import React from "react";
-import { Button, View, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
+import LottieView from "lottie-react-native";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
-import LottieView from "lottie-react-native";
 
 export default class NextBtn extends React.Component {
- componentDidMount();
-  {
-    this.nextBtn.play();
+  componentDidMount() {
+    this.NextBtn.play();
   }
 
-const Btn = ({ setRound, setClicked }) => {
-  const nextRound = () => {
-    setRound((currentValue) => {
-      if (currentValue >= 10) return currentValue;
-      return currentValue + 1;
-    });
-  };
-
-  return (
-    <LottieView
-      style={style.nextBtn}
-      ref={(animation) => {
-        this.nextBtn = animation;
-      }}
-      onPress={() => {
-        nextRound();
-        setClicked(false);
-      }}
-      source={require("../../style/animations/lottiePlay.json")}
-    />
-  );
-    }
-};
-
+  render() {
+    return (
+      <LottieView
+        style={style.NextBtn}
+        ref={(animation) => {
+          this.NextBtn = animation;
+        }}
+        source={require("../../style/animations/submitBtn.json")}
+      />
+    );
+  }
+}
 const style = StyleSheet.create({
-  nextBtn: {
-    height: hp("15%"),
-    width: wp("15%"),
+  NextBtn: {
+    height: hp("31%"),
+    width: wp("31%"),
+    alignItems: "center",
   },
 });
-
-export default NextBtn;
