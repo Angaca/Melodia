0bcb4f885b7ca06f3e9bafa0f91dfd6dab84b1b1
Andrea Catania <andreacatania91@gmail.com>
2021-08-04 14:48:42 +0100

    change double import

--- components/Results/Results.js
+++ components/Results/Results.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Platform, View, ImageBackground, StyleSheet } from "react-native";
+import { Platform } from "react-native";
 import AppStyle from "../../style/App.style";
 import {
   Text,
