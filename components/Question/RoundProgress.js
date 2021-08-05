c963ad10f93278cd87786631336c99b9bdb8f47f
Juliette Curran <juliettecurran@hotmail.com>
2021-08-05 20:52:14 +0100

    Removed an import and added marginBottom to Round

--- components/Question/RoundProgress.js
+++ components/Question/RoundProgress.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Text, View, StyleSheet } from "react-native";
-import AppStyle from "../../style/App.style";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 
 const RoundProgress = ({ round }) => {
   return <Text style={style.round}>Round {round}/10</Text>;
@@ -11,6 +14,8 @@ const style = StyleSheet.create({
     color: "black",
     fontSize: 16,
     textAlign: "center",
+    fontWeight: "600",
+    marginBottom: hp("3%"),
   },
 });
 
